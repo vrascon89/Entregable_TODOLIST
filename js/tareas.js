82f94311495749036a2de1be36a06a9615c091f3
Víctor Rascón <vrascon.sedano89@gmail.com>
2021-05-05 23:41:42 +0200

    array de tareas

---
+++ js/tareas.js
@@ -0,0 +1,31 @@
+let listaTareas = new Array();
+
+listaTareas = [
+    
+    {
+        idTarea: 1,
+        titulo: 'Ver Twitch a Mario',
+        prioridad:'optativo',
+    },
+    {
+        idTarea: 2,
+        titulo: 'Alquiler',
+        prioridad:'urgente',
+    },
+    {
+        idTarea: 3,
+        titulo: 'Ver videos de Victor Robles',
+        prioridad:'diario',
+    },
+    {
+        idTarea: 4,
+        titulo: 'Ir a Neoland',
+        prioridad:'diario',
+    },   
+    {
+        idTarea: 5,
+        titulo: 'Estudiar JavaScript',
+        prioridad:'urgente',
+    },
+
+]
